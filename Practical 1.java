2b46ba06551df01d5e2bf673e9d54a2ffc36477b
VanshitaR <95092468+VanshitaR@users.noreply.github.com>
2021-12-07 18:38:11 +0530

    Create Practical 1.java

---
+++ Practical 1.java
@@ -0,0 +1,89 @@
+To convert string into lowercase
+example 1:
+input ="ABcddE"
+output ="abcdde"
+ example 2:
+input ="LMNoppQQ"
+output ="lmnoppqq"
+  
+  solution-
+  import java.util.Scanner;
+public class Main{
+  public static void main(string[] args){
+    Scanner sc=new Scanner(System.in);
+ String s=sc.nextLine();
+  s=s.toLowerCase();
+  System.out.println(s);
+  
+  String s=sc.nextLine();
+  p=p.toLowercase();
+  System.out.println(p);
+  
+  
+  Reverse the string:
+  example 1:
+  input: s="GeeksforGeeks"
+  output: s="skeeGrofskeeG"
+  
+  solution:
+  
+  import java.util.Scanner:
+  public class Main{
+  public static void main(String[] args){
+  Scanner sv=new Scanner(System.in);
+  System.out.println("enter string name");
+  String s=sc.nextLine();
+  char[] arr=s.toCharArray;
+  String reverse=" ";
+  System.out.println("reverse string is");
+  for(int i=arr.length-1;i>=0;i--){
+  System.out.println(arr[i]);
+  }
+  }
+  }
+  
+  example on inheritance:
+  input:
+  1
+  2 4
+  output:
+  6 
+  8
+  20
+  
+  solution:
+  
+  import java.util.Scanner;
+  import java.io.*;
+  import java.lang.Math;
+  class cls1{
+  void add(int p,int q)
+  {
+  System.out.println(p+q);
+  }}
+  public class Solution {
+  public static void main(String[] args){
+  Scanner sc=new Scanner(System.in);
+  int t=sc.nextInt();
+  while(t-->0){
+  cls2 obj=new cls2();
+  int p=sc.nextInt();
+  int q=sc.nextInt();
+  obj.add(p,q);
+  obj.mul(p,q);
+  obj.task(p,q);
+  }}}
+  class cls2 extends cls1{
+  void mul(int p.intq)
+  { System.out.println(p*q);
+  }
+  void task(int p,int q)
+  { System.out.println((p*p)+(q*q));
+  }
+  }
+  
+
+  
+ 
+
+  
